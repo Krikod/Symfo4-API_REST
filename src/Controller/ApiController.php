7b28cb513a55e92c6ddce514ff42bda092ee2687
Krikod <kristen.kod@gmail.com>
2019-10-17 11:56:25 +0200

    district list per region - controller, region preselected still visible

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Region;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -22,6 +23,32 @@ class ApiController extends AbstractController
 
         return $this->render('api/index.html.twig', array(
             'mesRegions' => $mesRegions
+        ));
+
+    }/**
+     * @Route("/listeDepsParRegions", name="listeDepsParRegions")
+     */
+    public function listeDepsParRegions(Request $request, SerializerInterface $serializer)
+    {
+//        Je récupère la région sélectionnée dans le code
+        $codeRegion = $request->query->get('region');
+
+//        Récupérer les régions
+        $mesRegions = file_get_contents('https://geo.api.gouv.fr/regions');
+        $mesRegions = $serializer->deserialize($mesRegions, 'App\Entity\Region[]', 'json');
+
+        //      Je récup la liste des Deps
+        if ($codeRegion == null || $codeRegion == 'Toutes') {
+            $mesDeps = file_get_contents('https://geo.api.gouv.fr/departements');
+        } else {
+            $mesDeps = file_get_contents('https://geo.api.gouv.fr/regions/'.$codeRegion.'/departements');
+        }
+//        Décodage du json en array
+        $mesDeps = $serializer->decode($mesDeps, 'json');
+//dump($mesDeps);die();
+        return $this->render('api/listDepsParRegion.html.twig', array(
+            'mesRegions' => $mesRegions,
+            'mesDeps' => $mesDeps
         ));
     }
 }
--- src/Entity/Region.php
+++ src/Entity/Region.php
@@ -27,7 +27,7 @@ class Region
 
     public function getCode(): ?string
     {
-        return "n° " . $this->code;
+        return $this->code;
     }
 
     public function setCode(string $code): self
