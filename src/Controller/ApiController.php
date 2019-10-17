6db9ff3e26578d725f943a1f1855ebedaf6c90a4
Krikod <kristen.kod@gmail.com>
2019-10-17 10:38:23 +0200

    denormalize, deserialize

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Region;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -14,10 +15,13 @@ class ApiController extends AbstractController
     public function listeRegions(SerializerInterface $serializer)
     {
         $mesRegions = file_get_contents('https://geo.api.gouv.fr/regions');
-        $mesRegionsTab = $serializer->decode($mesRegions, 'json');
-//dump($mesRegionsTab);die();
+//        $mesRegionsTab = $serializer->decode($mesRegions, 'json');
+//        $mesRegionsObjet = $serializer->denormalize($mesRegionsTab, 'App\Entity\Region[]');
+        $mesRegions = $serializer->deserialize($mesRegions, 'App\Entity\Region[]', 'json');
+//dump($mesRegionsObjet);die();
+
         return $this->render('api/index.html.twig', array(
-            'mesRegions' => $mesRegionsTab
+            'mesRegions' => $mesRegions
         ));
     }
 }
